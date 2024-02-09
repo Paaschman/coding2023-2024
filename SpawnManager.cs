6545ea3daea73aeddbd3e5a275e01010e71a0acb
Paaschman <146866822+Paaschman@users.noreply.github.com>
2024-02-09 09:53:51 -0500

    Add files via upload

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -5,27 +5,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos;
-        public float startDelay = 2;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    public float startDelay = 2;
     private float repeatRate = 2;
+    private playercontroller playerControllerScript;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomObstacle", startDelay, repeatRate);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
 
-    
-}
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    void SpawnRandomObstacle()
+    void SpawnObstacle()
     {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
-
 }

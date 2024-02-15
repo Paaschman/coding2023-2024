07abc0681ea052b075aa538ab64d5f8c43a08293
Paaschman <146866822+Paaschman@users.noreply.github.com>
2024-02-15 15:11:09 -0500

    Add files via upload

--- SpawnManagerX.cs
+++ SpawnManagerX.cs
@@ -4,33 +4,31 @@ using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
-    public GameObject[] ballPrefabs;
+    public GameObject[] objectPrefabs;
+    private float spawnDelay = 2;
+    private float spawnInterval = 1.5f;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
-
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private PlayerControllerX playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("PrawnsObject", spawnDelay, spawnInterval);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
 
-    // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    // Spawn obstacles
+    void SpawnObjects ()
     {
-        int index = Random.Range(0, ballPrefabs.Length);
+        // Set random spawn location and random object index
+        Vector3 spawnLocation = new Vector3(30, Random.Range(5, 15), 0);
+        int index = Random.Range(0, objectPrefabs.Length);
 
-       
+        // If game is still active, spawn new object
+        if (!playerControllerScript.gameOver)
+        {
+            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        }
 
-        // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
     }
-
 }
